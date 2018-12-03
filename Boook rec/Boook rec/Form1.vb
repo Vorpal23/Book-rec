04bfdeb6010333a351e3d55796bc4b961f6ddce3
Jordan Rose <31141432+Vorpal23@users.noreply.github.com>
2018-12-03 11:50:15 -0500

    Book rec
    Book rec Project
    
    - Reader Class made with import function

---
+++ Boook rec/Boook rec/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim Reader(5) As Read
+        Dim i As Integer
+        For i = 1 To 86
+            Reader(i).Import("Z:\BookRecomendationProject\readerratings.txt", 2, i)
+        Next
+
+
+    End Sub
+End Class
---
+++ Boook rec/Boook rec/Reader.vb
@@ -0,0 +1,76 @@
+﻿Imports System.IO
+
+Public Class Read
+    Structure Human
+        Dim ratings() As Double
+        Dim name As String
+
+    End Structure
+    Dim rdr As Human
+    Public Property data As Human
+
+
+
+
+    Sub Import(path As String, readln As Integer, person As Integer)
+        Try
+            FileOpen(1, path, OpenMode.Input, OpenAccess.Read)
+            GoTo goodOpen
+        Catch ex As Exception
+            Try
+                FileClose(1)
+                FileOpen(1, path, OpenMode.Input, OpenAccess.Read)
+                GoTo goodOpen
+            Catch ex2 As Exception
+                MsgBox(ex2)
+            End Try
+
+        End Try
+
+
+goodOpen:
+        Dim Strlines(readln) As String
+
+
+        Try
+
+
+
+
+            For i As Integer = 1 To (person * 2) - 1
+                Strlines(1) = LineInput(1)
+                Strlines(2) = LineInput(2)
+
+            Next
+            rdr.name = Strlines(1)
+            ReDim rdr.ratings(UBound(Strlines(2).Split(" ")))
+            For i As Integer = 1 To UBound((Strlines(2).Split(" ")))
+                rdr.ratings(i) = Val(Strlines(2).Split(" ")(i))
+            Next
+            data = rdr
+
+
+
+        Catch ex As Exception
+            MsgBox("Out Of Range")
+
+        End Try
+
+    End Sub
+
+    Public Overrides Function ToString() As String
+        Return MyBase.ToString()
+    End Function
+
+    Public Overrides Function Equals(obj As Object) As Boolean
+        Return MyBase.Equals(obj)
+    End Function
+
+    Public Overrides Function GetHashCode() As Integer
+        Return MyBase.GetHashCode()
+    End Function
+
+    Protected Overrides Sub Finalize()
+        MyBase.Finalize()
+    End Sub
+End Class
